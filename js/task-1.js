040fe310ddfddbc4adf50851627c9634570cdf87
YuraHez <trisher1985@gmail.com>
2025-02-10 01:30:25 +0200

    tasks1,2,3 are done

--- js/task-1.js
+++ js/task-1.js
@@ -30,3 +30,4 @@ function slugify(title) {
   console.log(slugify("English for developer")); // "english-for-developer"
   console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
   console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+  
